44e9ce12df1d4e4d5da748bff458a7b2d137237f
khanhhuy123 <khanhhuy7591@gmail.com>
2024-03-24 00:01:03 +0100

    AddMethodSetNum

--- test2/src/Class99.java
+++ test2/src/Class99.java
@@ -4,4 +4,8 @@ public class Class99 {
     public Class99(int num1){
         System.out.println("hello world");
     }
+
+    public void setNum1(){
+        System.out.println("hello world");
+    }
 }
